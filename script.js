c0cc4a6419ec8b9a4ffe63825aa39f6ac56934a9
Mike Stay <30355933+pyrocto@users.noreply.github.com>
2020-04-01 18:26:47 -0600

    Code review suggested changes

--- script.js
+++ script.js
@@ -6,11 +6,128 @@ var speech = new webkitSpeechRecognition();
 var i = 0;
 var recognizing = true;
 commands = (speechToText) => {
-  let commandList = speechToText.split(" ");
-  if (commandList.includes("what") || commandList.includes("how") || commandList.includes("who")) {
-    let searchTerms = commandList.join("+")
-    window.location.href="https://www.google.com/search?q=" + searchTerms;
+  let orders = speechToText.toLowerCase().split(" ");
+  let command = orders[0];
+  let rest = orders.slice(1);
+  if (command === "search") {
+    let searchTerms = orders.join("+")
+    window.location.replace("https://www.google.com/search?q=" + searchTerms);
   }
+  
+  if (command === "scriptures") {
+    let searchTerms = rest;
+    let chapter = 0;
+    let book = "";
+    let category = "";
+    let work = "";
+    // Check last element of rest to see if it's a number  last = +rest[rest.length - 1]
+    //   If !Number.isNaN(last), set chapter to last and set searchTerms to searchTerms.slice(0, len-1)     
+    // Set bookName to searchTerms.join(" ")
+    // switch (bookName) {
+    //   case "1st Nephi": book = "1-ne/"; category = "scriptures/"; work = "bofm/"; break;
+    //   case "2nd Nephi": Book = "2-ne/"; category = "scriptures/"; work = "bofm/"; break;
+    //   ...  
+    // }
+    // If chapter > 0, build a URL to the chpater
+    // Otherwise, build a URL to the book
+    // set location.href to the link
+
+    /* for (i = 0; i <= 3; i++) {
+      if (book === "") {
+        if ((index = searchTerms.find("1st")) > -1) {
+          searchTerms[index] = "";
+          category = "scriptures"
+          bookConstruct[0] = "1";
+        }
+        if (searchTerms.includes("2nd")) {
+          searchTerms.replace("2nd", "");
+          category = "scriptures"
+          bookConstruct[0] = "2";
+        }
+        if (searchTerms.includes("3rd")) {
+          searchTerms.replace("3rd", "");
+          category = "scriptures"
+          bookConstruct[0] = "1";
+        }
+        if (searchTerms.includes("4th")) {
+          searchTerms.replace("4th", "");
+          category = "scriptures"
+          bookConstruct[0] = "4";
+        }
+        if (searchTerms.includes("nephi")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "-ne/";
+        }
+        if (searchTerms.includes("jacob")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "jacob/";
+        }
+        if (searchTerms.includes("enos")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "enos/";
+        }
+        if (searchTerms.includes("jarom")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "jarom";
+        }
+        if (searchTerms.includes("omni")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "omni/";
+        }
+        if (searchTerms.includes("words")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "w-of-m/";
+        } else if (searchTerms.includes("mormon")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "morm/";
+        }
+        if (searchTerms.includes("mosiah")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "mosiah/";
+        }
+        if (searchTerms.includes("alma")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "alma/";
+        }
+        if (searchTerms.includes("helaman")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "hel/";
+        }
+        if (searchTerms.includes("ether")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "ether/";
+        }
+        if (searchTerms.includes("moroni")) {
+          category = "scriptures"
+          work = "bofm/"
+          bookConstruct[1] = "moroni/";
+        }
+        book = bookConstruct.join();
+      } else {
+        for (i = 1; i <= 10; i++) {
+          a = i.toString;
+          if (searchTerms.includes(a)) {
+            searchTerms.replace(a, "");
+            chapterConstruct.push[a];
+          }
+        }
+        chapter = chapterConstruct.join();
+      }
+
+      window.location.href = ("gospellibrary://content/" + category + work + book + chapter);
+    }
+    */
   /*if(commandList.includes("maps" || "directions")){
     //search google maps for speechToText;
   }
